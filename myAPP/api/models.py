d11c070c3c3b5dcf580ba6c828d70b9f3db8f513
MyLabPC <512450113@qq.com>
2020-10-30 20:38:18 +0800

    tryPycharm

--- myAPP/api/models.py
+++ myAPP/api/models.py
@@ -10,5 +10,5 @@ class MyTest(models.Model):
     id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=80, blank=True, null=True)
     patient = models.CharField(max_length=80, blank=True, null=True)
-    type = models.CharField(max_length=20, blank=True, null=True)
+    type = models.CharField(max_length=30, blank=True, null=True)
     detail = models.JSONField() # default=my_default
